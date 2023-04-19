0d8f1fe5b50f5b2a3f7cf8f619dcbdd2c8d84746
goncalveshigino <goncalveshigino20@gmail.com>
2023-04-19 08:58:08 +0100

    rotas em um path especifico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { PagesRoutingModule } from './pages/pages.routing';
 import { AuthRoutingModule } from './auth/auth.routing';
-
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 
-
-
-
-
 const routes: Routes = [
 
- 
-
- 
-
+  //path: '/dashboard' PagesRouting
+  //path: '/auth' AuthRouting
+  { path: '',redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent },
 
 ];
